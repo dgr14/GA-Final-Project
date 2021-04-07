79b20d525f08153a4e527f7cb7effc71a0aedd73
Darius Raazi <dariusraazi@icloud.com>
2021-04-07 16:20:21 -0600

    making my styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import React from 'react'
 import styled from 'styled-components'
+import Navbar from './components/Navbar'
+
+const AppContainer = styled.main`
+  height: 100%;
+  width: 100%;
+`
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AppContainer>
+      <Navbar />
+    </AppContainer>
   );
 }
 
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            {/* <Nav>
+                <NavBarContainer>
+                    <NavLogo>dolla</NavLogo>
+                </NavBarContainer>
+            </Nav> */}
+        </>
+    )
+}
+
+export default Navbar
